f6df0ef6ac42711f1ac59b3fcf03a44a54390661
TKO <28767546+TKO-Cuber@users.noreply.github.com>
2018-01-01 21:44:27 -0500

    Add files via upload

---
+++ detect.js
@@ -0,0 +1,3 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    window.location = "https://tko-cuber.github.io/repo-mobile/";
+}
